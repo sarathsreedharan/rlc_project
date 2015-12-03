8e6fcc26f432d3a3a8705207ac79b5148fc74e87
sarathsreedharan <sarath.s.mec@gmail.com>
2015-12-03 15:55:33 -0700

    disabling emote

--- src/plan_executor.py
+++ src/plan_executor.py
@@ -2,8 +2,9 @@ from DmpLibrary import *
 import sys, os
 import subprocess
 
-marker_map = {'cup': 'ar_marker_8', 'ice_cube': 'ar_marker_'}
+marker_map = {'cup': 'ar_marker_8', 'ice_cube': 'ar_marker_10', 'default_region':'ar_marker_9'}
 baxter_emotion_map = {"happy":"happy.png", "sad": "sad.png", "quizzical":"quizzical.png"}
+dmp_map = 
 def baxter_emote(emotion):
     current_dir = os.path.dirname(os.path.realpath(__file__))
     image_dir = os.path.join(current_dir, '../images/')
@@ -24,7 +25,7 @@ def traslate_obj_to_markers(obj1, obj2):
         marker_2 = marker_map[obj2]
     return marker_1, marker_2
 
-def convert_action_to_dmp(action):
+def convert_action_to_dmp(action, relevant_plan = []):
     return dmp_name
 
 if __name__ == "__main__":
@@ -37,16 +38,22 @@ if __name__ == "__main__":
             if action[0] != ';':
                 plan.append(action.strip())
                 ## TODO convert regions to markers
-    baxter_emote("focus")
-    for action in plan:
-        action_name, arg1, arg2 = action.split(1)
-        dmp_name = convert_action_to_dmp(action_name)
-        marker1, marker2 = 
+    #baxter_emote("focus")
+    for p_index in range(len(plan)):
+        action = plan[p_index]
+        relevant_plan = []
+        for act_2 in plan[p_index:]:
+            if act_2 == action:
+                break
+            relevant_plan.append(act_2)
+        action_name, arg1, arg2 = action.split()
+        dmp_name = convert_action_to_dmp(action_name, relevant_plan)
+        marker1, marker2 = traslate_obj_to_markers(arg1, arg2)
         status = dmp_lib.execute(action_name, arg1, arg2)
         if not status:
-            baxter_emote("sad")
+            #baxter_emote("sad")
             print "Unexpected error during plan execution"
             exit(1)
-    baxter_emote("happy")
+    #baxter_emote("happy")
     print "Plan executed succesfully, hopefully no one was hurt too bady"
     exit(0)
